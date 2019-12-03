48ee726edd4056093a03fdd6cfc999bc66750c60
Eric Lifka <eric.lifka@genesys.com>
2019-12-02 21:41:42 -0500

    day 01 part 02

--- src/01.js
+++ src/01.js
@@ -1,9 +1,25 @@
 import { getLines } from './input-helpers.js'
 import { parseInteger } from './conversions.js'
 
-let data = getLines('01')
+function getFuel(weight) {
+  let fuel = Math.floor(weight / 3) - 2
+  if (fuel <= 0) {
+    return 0;
+  } else {
+    return fuel + getFuel(fuel)
+  }
+}
+
+let data1 = getLines('01')
   .map(parseInteger)
   .map(i => Math.floor(i / 3) - 2)
-  .reduce((prev, cur) => prev + cur, 0);
+  .reduce((prev, cur) => prev + cur, 0)
 
-console.log(data)
+console.log(data1)
+
+let data2 = getLines('01')
+  .map(parseInteger)
+  .map(i => getFuel(i))
+  .reduce((prev, cur) => prev + cur, 0)
+
+console.log(data2)
