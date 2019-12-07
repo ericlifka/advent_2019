e61416b768b3db3303adb2a01277eeebfd4f4ba5
Eric Lifka <eric.lifka@genesys.com>
2019-12-07 13:37:33 -0500

    day 06 cleanup

--- src/06.js
+++ src/06.js
@@ -1,9 +1,8 @@
 import { getLines } from './input-helpers.js'
-import { splitter } from './converters.js'
+import { reduceSum, splitter } from './converters.js'
 
-let links =
-getLines('06')
-  .map(splitter(')'))
+let links = getLines('06').map(splitter(')'))
+  , nodes = buildGraph()
 
 const countOrbits = (node, count = 0) =>
   !node.parent ? count
@@ -12,44 +11,34 @@ const countOrbits = (node, count = 0) =>
 function buildGraph() {
   let nodes = { }
 
-  links.forEach( ([parent, child]) => {
-    if (!nodes[parent])
-      nodes[parent] = { node: parent }
+  links.forEach(([ parent, child ]) =>
+    ( nodes[parent] = { node: parent }
+    , nodes[child] = { node: child }))
 
-    if (!nodes[child])
-      nodes[child] = { node: child }
-  })
-
-  links.forEach( ([parent, child]) =>
+  links.forEach(([ parent, child ]) =>
     nodes[child].parent = nodes[parent])
 
   return nodes
 }
 
-export const day06part1 = () => {
-  let nodes = buildGraph()
-
-  let orbits = Object.keys(nodes).map( name =>
-    countOrbits(nodes[name]))
-
-  return orbits.reduce((sum, cur) =>
-    sum + cur
-  , 0)
-}
+export const day06part1 = () =>
+  Object
+    .keys(nodes)
+    .map( name => countOrbits(nodes[name]))
+    .reduce(reduceSum, 0)
 
 export const day06part2 = () => {
-  let nodes = buildGraph()
-    , you = nodes['YOU']
+  let you = nodes['YOU']
     , santa = nodes['SAN']
     , youCount = 0
     , santaCount = 0
-  while (you.parent) {
-    you = you.parent
+
+  while (you = you.parent)
     you.youCount = youCount++
-  }
-  while (!santa.youCount) {
-    santa = santa.parent
+
+  while (  !santa.youCount
+        && (santa = santa.parent))
     santa.santaCount = santaCount++
-  }
+
   return santa.youCount + santa.santaCount
 }
