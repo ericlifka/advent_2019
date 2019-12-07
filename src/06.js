4b506d5fe2b3b29167de31db90d44150d0797d48
Eric Lifka <eric.lifka@genesys.com>
2019-12-07 13:21:41 -0500

    day 06 part 2

--- src/06.js
+++ src/06.js
@@ -9,7 +9,7 @@ const countOrbits = (node, count = 0) =>
   !node.parent ? count
                : countOrbits(node.parent, count + 1)
 
-export const day06part1 = () => {
+function buildGraph() {
   let nodes = { }
 
   links.forEach( ([parent, child]) => {
@@ -23,6 +23,12 @@ export const day06part1 = () => {
   links.forEach( ([parent, child]) =>
     nodes[child].parent = nodes[parent])
 
+  return nodes
+}
+
+export const day06part1 = () => {
+  let nodes = buildGraph()
+
   let orbits = Object.keys(nodes).map( name =>
     countOrbits(nodes[name]))
 
@@ -31,4 +37,19 @@ export const day06part1 = () => {
   , 0)
 }
 
-export const day06part2 = () => null
+export const day06part2 = () => {
+  let nodes = buildGraph()
+    , you = nodes['YOU']
+    , santa = nodes['SAN']
+    , youCount = 0
+    , santaCount = 0
+  while (you.parent) {
+    you = you.parent
+    you.youCount = youCount++
+  }
+  while (!santa.youCount) {
+    santa = santa.parent
+    santa.santaCount = santaCount++
+  }
+  return santa.youCount + santa.santaCount
+}
