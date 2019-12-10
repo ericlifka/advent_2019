3d5403f20874a7246c0c8207be8e5fb521bcb7e6
Eric Lifka <eric.lifka@genesys.com>
2019-12-09 22:01:42 -0500

    day09part2

--- src/09.js
+++ src/09.js
@@ -3,7 +3,8 @@ import { runProgram } from "./opcode-computer.js"
 
 let program = getNumberArray('09')
 
-export const day09part1 = () => JSON.stringify(
-  runProgram(program, [ 1 ]) )
+export const day09part1 = () =>
+  runProgram(program, [ 1 ])
 
-export const day09part2 = () => null
+export const day09part2 = () =>
+  runProgram(program, [ 2 ])
